becabab28f401f44fc5f7fe1896503e70a0487d6
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-02-03 21:10:09 -0600

    flexbox

--- App.js
+++ App.js
@@ -1,30 +1,22 @@
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text style={styles.box1}>
+          Hi every one
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
+        <Text style={styles.box2}>
+          Hi every one
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        <Text style={styles.box3}>
+          Hi every one
         </Text>
       </View>
     );
@@ -36,16 +28,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#000000',
+    backgroundColor: '#fff',
+    flexDirection: 'row'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  box1: {
+    backgroundColor: 'green'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  box2: {
+    backgroundColor: 'blue'
   },
+  box3: {
+    backgroundColor: 'purple'
+  }
 });

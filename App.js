5206b4f3ef154c844b1518468f64b3f6f124e422
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-02-03 23:46:52 -0600

    artistbox

--- App.js
+++ App.js
@@ -2,22 +2,35 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
 
 export default class App extends Component<{}> {
   render() {
+    const img = 'https://avatars0.githubusercontent.com/u/27437337?s=460&v=4'
     return (
       <View style={styles.container}>
-        <Text style={styles.box1}>
-          Hi every one
-        </Text>
-        <Text style={styles.box2}>
-          Hi every one
-        </Text>
-        <Text style={styles.box3}>
-          Hi every one
-        </Text>
+        <View style={styles.artistBox}>
+          <Image style={styles.image} source={{ uri: img }} />
+          <View style={styles.info}>
+            <Text style={styles.name}>Juan Luis Rojas</Text>
+            <View style={styles.row}>
+              <View style={styles.iconContainer}>
+                <Icon name="ios-heart-outline" size={30} color="gray" />
+                <Text style={styles.count}>200</Text>
+              </View>
+              <View style={styles.iconContainer}>
+                <Icon name="ios-chatboxes-outline" size={30} color="gray" />
+                <Text style={styles.count}>10</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -26,18 +39,38 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgray',
+    paddingTop: 50
+  },
+  artistBox: {
+    backgroundColor: 'white',
     flexDirection: 'row'
   },
-  box1: {
-    backgroundColor: 'green'
+  image: {
+    width: 150,
+    height: 150
   },
-  box2: {
-    backgroundColor: 'blue'
+  info: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   },
-  box3: {
-    backgroundColor: 'purple'
+  name: {
+    fontSize: 20,
+    marginTop: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 40,
+    marginTop: 15
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  count: {
+    color: 'gray'
   }
 });

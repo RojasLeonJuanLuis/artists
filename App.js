d1d225cdff71f918c2aa3713d625cd6a72f94b80
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-02-05 10:56:24 -0600

    artists box components

--- App.js
+++ App.js
@@ -6,31 +6,26 @@ import {
   Image
 } from 'react-native';
 
+import ArtistsBox from './components/artists-box'
+
 import Icon from 'react-native-vector-icons/Ionicons';
 
 
 
 export default class App extends Component<{}> {
   render() {
-    const img = 'https://avatars0.githubusercontent.com/u/27437337?s=460&v=4'
+    const artist = {
+      name: 'Juan Luis Rojas',
+      image: 'https://static.platzi.com/media/files/bowie_a927fdf3-b321-4a5c-99ca-239cc86c57bc.png',
+      likes: 200,
+      comments: 10
+    }
     return (
       <View style={styles.container}>
-        <View style={styles.artistBox}>
-          <Image style={styles.image} source={{ uri: img }} />
-          <View style={styles.info}>
-            <Text style={styles.name}>Juan Luis Rojas</Text>
-            <View style={styles.row}>
-              <View style={styles.iconContainer}>
-                <Icon name="ios-heart-outline" size={30} color="gray" />
-                <Text style={styles.count}>200</Text>
-              </View>
-              <View style={styles.iconContainer}>
-                <Icon name="ios-chatboxes-outline" size={30} color="gray" />
-                <Text style={styles.count}>10</Text>
-              </View>
-            </View>
-          </View>
-        </View>
+        <ArtistsBox artist={artist} />
+        <ArtistsBox artist={artist} />
+        <ArtistsBox artist={artist} />
+        <ArtistsBox artist={artist} />
       </View>
     );
   }
@@ -41,37 +36,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'lightgray',
     paddingTop: 50
-  },
-  artistBox: {
-    backgroundColor: 'white',
-    flexDirection: 'row'
-  },
-  image: {
-    width: 150,
-    height: 150
-  },
-  info: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  name: {
-    fontSize: 20,
-    marginTop: 10,
-    color: '#333'
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: 40,
-    marginTop: 15
-  },
-  iconContainer: {
-    flex: 1,
-    alignItems: 'center'
-  },
-  count: {
-    color: 'gray'
   }
 });
